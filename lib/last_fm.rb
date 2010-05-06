713feb4631591eb1f09384ceab9506cbf0d3f901
speric <eric@prudentiadigital.com>
2010-05-06 15:09:44 -0400

    Clearer explanation

--- lib/last_fm.rb
+++ lib/last_fm.rb
@@ -3,17 +3,18 @@ require 'rexml/document'
 
 class LastFm
   
-  #takes your last.fm username as the parameter
+  # takes your last.fm username as the parameter
   def initialize(username)
     @startpoint = "http://ws.audioscrobbler.com/2.0/user/#{username}/"
   end
   
-  #get the topartists for the last 3, 6, 9 or 12 months.  Passing no parameter returns the top artists overall
+  # topartists for the last 3, 6, 9 or 12 months.  
+  # Passing no parameter returns the top artists overall
   def topartists(duration = 0)
     send_request("topartists.xml?period=#{duration}month", "topartists/artist")
   end
 
-  #latest weekly aritst chart
+  # latest weekly aritst chart
   def weeklyartistchart
     send_request("weeklyartistchart.xml", "weeklyartistchart/artist")
   end
